2124eac8746c49b293022bd6863c6186d543cb4b
Josue Valverde & Tim Kellogg <student@epicodus.com>
2015-08-19 10:02:29 -0700

    add working method and spec for returning all patrons

--- lib/patron.rb
+++ lib/patron.rb
@@ -6,4 +6,17 @@ class Patron
     @total_fine = attributes[:total_fine]
     @id         = attributes[:id]
   end
+
+
+  define_singleton_method(:all) do
+    returned_patrons = DB.exec("SELECT * FROM patrons;")
+    patrons = []
+    returned_patrons.each() do |returned_patron|
+      name       = returned_patron['name']
+      total_fine = returne_patron['total_fine'].to_i()
+      id         = returned_patron['id'].to_i()
+      patrons.push(Patron.new({:name => name, :total_fine => total_fine, :id => id}))
+    end
+    patrons
+  end
 end
--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -6,9 +6,16 @@ describe Patron do
   before do
     @name = Patron.new({ :name => 'Patron', :total_fine => 9, :id => nil })
   end
+
   describe '#name' do
     it 'should return the name of the patron' do
       expect(@name.name).to eq 'Patron'
+    end
+  end
+
+  describe '.all' do
+    it 'should be empty at first' do
+      expect(Patron.all()).to eq []
     end
   end
 end
