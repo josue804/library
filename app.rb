e1dd07b962cbceb8a20fb74d557f9aa337d6ecc1
Josue Valverde & Tim Kellogg <student@epicodus.com>
2015-08-19 16:21:09 -0700

    add personal styling and routing templates.

--- app.rb
+++ app.rb
@@ -8,6 +8,30 @@ require './lib/checkout'
 DB = PG.connect({:dbname => 'library'})
 
 
-get '/'  do
-	erb :index
+get('/') do
+	erb(:index)
+end
+
+get('/patrons/new') do
+	erb(:patrons_form)
+end
+
+get('/patrons') do
+	erb(:patrons)
+end
+
+post('/patrons') do
+	erb(:patrons)
+end
+
+get('/patrons/:id/checkouts/new') do
+	erb(:checkouts_form)
+end
+
+get('/patrons/:id/checkouts') do
+	erb(:checkouts)
+end
+
+post('/patrons/:id/checkouts') do
+	erb(:checkouts)
 end
