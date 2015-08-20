51a4479bb14ba824c1a77c8852faa23e5361b973
Josue Valverde & Tim Kellogg <student@epicodus.com>
2015-08-20 11:15:39 -0700

    add routing for displaying a patron's personal page.

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'sinatra/reloader'
 require './lib/book'
 require './lib/patron'
 require './lib/checkout'
+require 'pry'
 
 DB = PG.connect({:dbname => 'library'})
 
@@ -17,11 +18,36 @@ get('/patrons/new') do
 end
 
 get('/patrons') do
+	@patrons = Patron.all()
 	erb(:patrons)
 end
 
 post('/patrons') do
+	name = params.fetch('name')
+	patron = Patron.new({:id => nil, :name => name, :total_fine => 0})
+	patron.save()
+	@patrons = Patron.all()
 	erb(:patrons)
+end
+
+get('/patrons/:Invalid_login_info') do
+	puts 'got here'
+	@patrons = Patron.all()
+	@message = params.fetch('Invalid_login_info')
+	@message.gsub!('_', ' ')
+	@message.gsub!(':', '')
+	erb(:patrons)
+end
+
+post('/patrons/login') do
+	if params['password'] == 'password'
+		@id = params.fetch('id')
+		@
+		erb(:patron)
+	else
+		message = 'Invalid login info'
+		redirect('/patrons/:Invalid_login_info')
+	end
 end
 
 get('/patrons/:id/checkouts/new') do
